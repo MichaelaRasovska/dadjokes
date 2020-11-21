538850dbcfd6d372f477d22a8123ed83dfd90145
Michaela Rasovska <michaela.rasovska@gmail.com>
2020-11-21 11:54:10 +0100

    Úkol 14.

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './index.html';
+import './style.css';
 
-render(
-  <>
-    <header>
-      <h1>React Starter</h1>
-    </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app'),
-);
+const App = () => {
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+
+  return (
+    <div class="container">
+      <div class="joke">
+        <div class="joke__body">
+          <div class="joke__user">
+            <img class="user-avatar" src="assets/img/user01.png" />
+            <p class="user-name">Neroxx</p>
+          </div>
+
+          <p class="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div class="joke__likes">
+          <button
+            id="btn-up"
+            class="btn-like btn-like--up"
+            onClick={() => {
+              setLikes(likes + 1);
+            }}
+          ></button>
+          <span id="likes-up" class="likes-count likes-count--up">
+            {likes}
+          </span>
+          <button
+            id="btn-down"
+            class="btn-like btn-like--down"
+            onClick={() => {
+              setDislikes(dislikes + 1);
+            }}
+          ></button>
+          <span id="likes-down" class="likes-count likes-count--down">
+            {dislikes}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
